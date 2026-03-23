7f563719b3c1d0b127ef9381bbc81bc0e3d61c02
haoye123 <QQQQ3314855015@163.com>
2026-03-23 09:33:08 +0800

    创建了对应的卷model来存储数据

---
+++ app/src/main/java/com/cwriter/data/model/Volume.kt
@@ -0,0 +1,19 @@
+package com.cwriter.data.model
+
+import java.util.Date
+
+/**
+ * 卷数据模型
+ * 对应 Vue3 代码中的 volume 对象
+ */
+data class Volume(
+    val id: String = "",
+    val name: String = "",
+    val title: String = "",
+    val description: String = "",
+    val order: Int = 0,
+    var chapterCount: Int = 0,
+    var wordCount: Int = 0,
+    val createdAt: String = Date().toISOString(),
+    val updatedAt: String = Date().toISOString()
+)
